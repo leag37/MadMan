44c85e4d5666a78aac042411bf98bb67f730559a
Gael Huber <gael.scott.huber@gmail.com>
2014-03-14 20:39:41 -0700

    Adding a system requirements file.

---
+++ MadMan/include/core/MMSystem.h
@@ -0,0 +1,39 @@
+//*************************************************************************************************
+// Title: MMSystem.h
+// Author: Gael Huber
+// Description: Exposes base system requirements, typedefs, and header files
+//*************************************************************************************************
+#ifndef __MMSYSTEM_H__
+#define __MMSYSTEM_H__
+
+namespace MadMan
+{
+	/** \addtogroup Core
+	*	@{
+	*/
+
+	//*********************
+	// Includes
+	//*********************
+	#include <cassert>
+	#include <cfloat>
+	#include <cmath>
+	#include <cstdio>
+	#include <cstdlib>
+	#include <cstring>
+
+	//*********************
+	// Typedefs
+	//*********************
+	typedef unsigned int	MUINT;
+	typedef int				MINT;
+	typedef unsigned long	MULONG;
+	typedef long			MLONG;
+	typedef float			MFLOAT;
+
+
+	/** @} */
+
+}	// Namespace
+
+#endif // __MMSYSTEM_H__
